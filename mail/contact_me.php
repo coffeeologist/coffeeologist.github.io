2905e8c2e1602c31dc8d575a6c539cfb23202a7a
coffeeologist <liuamy000@gmail.com>
2019-01-09 23:36:42 -0500

    Minor updates & Contact form fixes

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -6,7 +6,7 @@ if(empty($_POST['name'])      ||
    empty($_POST['message'])   ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-   echo "No arguments Provided!";
+   echo "Please fill in the required fields!";
    return false;
    }
    
@@ -16,11 +16,12 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'jiachen5@andrew.cmu.edu'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'jiachenamyliu@gmail.com';
 $email_subject = "Personal website contact form  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@google.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@gmail.com\n";
 $headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
+mail($to,$email_subject,$email_body,$headers) or die("Error!");
+echo "Thank you!";
 return true;         
 ?>
